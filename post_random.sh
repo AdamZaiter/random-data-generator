4d081daaf058dca2a539b5029ed9fd7d5915e25e
Adam Zaiter <zaiter.adam7@gmail.com>
2020-04-30 16:19:36 +0200

    example script for posting data from .json to a database

--- post_random.sh
+++ post_random.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # Shell script for populating a database
-# First argument is for the specific route
+# First argument is for the input file with the data
+# Second argument is for the specific route
 
-jq -c '.[]' random_data.json | while read i; do
+jq -c '.[]' $1 | while read i; do
     curl --header "Content-Type: application/json" \
     --request POST \
     --data $i \
-    $HOST/$1 
+    $HOST/$2 
   done
